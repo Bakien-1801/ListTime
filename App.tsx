df8350b9335de6cd1e2c47e8df4d5c4af468bf08
Bakien-1801 <nguyensonba_t64@hus.edu.vn>
2023-02-26 23:12:05 +0700

    first commit

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text> Xin chao anh em nha</Text>
+    </View>
+  );
+};
+
+export default App;
---
+++ src/Screens/SignIn/styles.tsx
@@ -0,0 +1,96 @@
+import { StyleSheet } from "react-native";
+
+const createStyles = () =>
+  StyleSheet.create({
+    view: {
+      //marginTop: 60,
+      // marginHorizontal: 20,
+      flex: 1,
+      alignSelf: "center",
+      backgroundColor: "white",
+    },
+    image1: {
+      flex: 0.5,
+      justifyContent: "flex-end",
+      // backgroundColor: 'pink'
+    },
+    textWelcome: {
+      fontSize: 50,
+      textAlign: "center",
+      color: "#fff",
+      fontFamily: FONTS.vanSansBlack,
+    },
+    text1: {
+      fontSize: 14,
+      margin: 15,
+      marginHorizontal: 10,
+      textAlign: "center",
+      fontFamily: FONTS.vanSansRegular,
+      fontWeight: "bold",
+    },
+    styletext: {
+      fontSize: 16,
+      marginHorizontal: 20,
+      marginTop: 10,
+      fontFamily: FONTS.vanSansSemiBold,
+    },
+    styleTT: {
+      flexDirection: "row",
+      borderWidth: 1,
+      marginTop: 10,
+      //height: 45,
+      marginHorizontal: 20,
+      padding: 10,
+      borderRadius: 5,
+      alignItems: "center",
+    },
+    text24: {
+      marginLeft: 10,
+      width: "84%",
+      fontSize: 14,
+    },
+    text21: {
+      marginLeft: 10,
+      width: "100%",
+      fontSize: 14,
+    },
+    btnForgot: {
+      alignItems: "center",
+      marginTop: 15,
+      marginRight: "3%",
+      fontWeight: "100",
+    },
+    text23: {
+      fontSize: 16,
+      fontFamily: FONTS.vanSansMedium,
+    },
+    checkbox: {
+      fontFamily: FONTS.vanSansMedium,
+      width: "42%",
+      //backgroundColor:"red"
+    },
+    btn2: {
+      width: 350,
+      height: 45,
+      alignSelf: "center",
+      marginHorizontal: 20,
+      justifyContent: "center",
+      alignItems: "center",
+      borderRadius: 10,
+      backgroundColor: "#7445f6",
+      marginTop: 65
+    },
+    text22: {
+      fontSize: 20,
+      width: 350,
+      padding: 5,
+      //backgroundColor: "#7445f6",
+      height: "100%",
+      color: "#FFFFFF",
+      textAlign: "center",
+      fontFamily: FONTS.vanSansBold,
+      
+    },
+  });
+
+export default createStyles;
